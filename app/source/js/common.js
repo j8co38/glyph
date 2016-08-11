25bdc359644899dfe40432f5e841b16deb47e89f
yuiko <j8co38@gmail.com>
2016-08-11 15:41:47 +0900

    キャプション右バー追加

--- app/source/js/common.js
+++ app/source/js/common.js
@@ -1,4 +1,13 @@
 	//------------------svg-anime-----------------------
-setTimeout(function() {
-	$('#svg-anime-wrap').fadeOut();
-}, 4000);
+$(function(){
+	setTimeout(function() {
+		$('#svg-anime-wrap').fadeOut();}, 3500);
+	setTimeout(function() {
+		$('#container').fadeIn();
+	}, 3800);
+
+	// $("#container").css({opacity:'0'});
+	// setTimeout(function(){
+	// 	$("#container").stop().animate({opacity:'1'},3500);
+	// },3000);
+});
