6b3db0147093f19ec7711a80047e6ecefae3c020
yuiko <j8co38@gmail.com>
2016-08-13 15:42:56 +0900

    firefoxに対応

--- app/source/js/common.js
+++ app/source/js/common.js
@@ -2,9 +2,4 @@
 $(function(){
 	setTimeout(function() {
 		$('#svg-anime-wrap').fadeOut();}, 3500);
-
-	// $("#container").css({opacity:'0'});
-	// setTimeout(function(){
-	// 	$("#container").stop().animate({opacity:'1'},3500);
-	// },3000);
 });
